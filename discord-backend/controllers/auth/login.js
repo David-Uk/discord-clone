cd19e52cf92869fdba1b61c354f62e790da9dfd8
David Ukelere <david.ukelere291@gmail.com>
2024-09-20 12:17:43 +0100

    Added login route

--- discord-backend/controllers/auth/login.js
+++ discord-backend/controllers/auth/login.js
@@ -1,5 +1,22 @@
+const User = require('../../models/user')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
 const login = async (req, res) => {
-    res.json('Login')
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email: email.toLowerCase() });
+
+        if (user && (await bcrypt.compare(password, user.password))) {
+            const token = jwt.sign(user.email, process.env.SECRET)
+            return res.status(201).json({ userId: user._id, email, token, username: user.username })
+        }
+        else return res.status(400).json({ error: 'Wrong login details' })
+
+    }
+    catch (err) {
+        return res.status(500).json(err.message)
+    }
 }
 
 module.exports = login;
