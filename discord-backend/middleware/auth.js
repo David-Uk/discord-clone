70a4da2ceda8efd1b31d4d164c2a6f3816991efc
David Ukelere <david.ukelere291@gmail.com>
2024-10-03 00:50:14 +0100

    Added frontend auth routes

--- discord-backend/middleware/auth.js
+++ discord-backend/middleware/auth.js
@@ -1,21 +1,22 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
 const config = process.env;
 
 const verifyToken = (req, res, next) => {
-    let token = req.body.token || req.query.token || req.headers.authorization;
+  let token = req.body.token || req.query.token || req.headers["authorization"];
 
-    if (!token) return res.status(403).json('Token required')
+  if (!token) {
+    return res.status(403).send("A token is required for authentication");
+  }
+  try {
+    token = token.replace(/^Bearer\s+/, "");
+    const decoded = jwt.verify(token, config.TOKEN_KEY);
+    req.user = decoded;
+  } catch (err) {
+    return res.status(401).send("Invalid Token");
+  }
 
-    try {
-        token = token.replace(/^Bearer\s+/, '')
-        const decoded = jwt.verify(token, config.SECRET)
-        req.user = decoded;
-    } catch (error) {
-        return res.status(401).send('Invalid token')
-    }
-
-    return next();
-}
+  return next();
+};
 
 module.exports = verifyToken;
--- discord-backend/routes/authRoutes.js
+++ discord-backend/routes/authRoutes.js
@@ -1,12 +1,35 @@
-const express = require('express')
-const { controller } = require('../controllers/auth/authController')
-const { registerSchema, loginSchema } = require('../validator/authValidator')
-// const { valid } = require('joi')
-const validator = require('express-joi-validation').createValidator({})
+const express = require("express");
+const router = express.Router();
+const authControllers = require("../controllers/auth/authControllers");
+const Joi = require("joi");
+const validator = require("express-joi-validation").createValidator({});
+const auth = require("../middleware/auth");
 
-const router = express.Router()
+const registerSchema = Joi.object({
+  username: Joi.string().min(3).max(12).required(),
+  password: Joi.string().min(6).max(12).required(),
+  mail: Joi.string().email().required(),
+});
 
-router.post('/register', validator.body(registerSchema), controller.register)
-router.post('/login', validator.body(loginSchema), controller.login)
+const loginSchema = Joi.object({
+  password: Joi.string().min(6).max(12).required(),
+  mail: Joi.string().email().required(),
+});
+
+router.post(
+  "/register",
+  validator.body(registerSchema),
+  authControllers.controllers.postRegister
+);
+router.post(
+  "/login",
+  validator.body(loginSchema),
+  authControllers.controllers.postLogin
+);
+
+// test route to verify if our middleware is working
+router.get("/test", auth, (req, res) => {
+  res.send("request passed");
+});
 
 module.exports = router;
